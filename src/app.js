7988791793ecb1d5d16edd641721fa8583bfecd5
Gustavo Menezes <gustavo.menezes@bistwobis.com.br>
2024-08-23 15:47:58 -0300

    creating new routes and using json

--- src/app.js
+++ src/app.js
@@ -2,8 +2,23 @@ import express from "express";
 
 const app = express();
 
+const livros = [
+    {
+        id: 1,
+        titulo: "Casais inteligentes enriquecem juntos"
+    },
+    {
+        id:2,
+        titulo: "A cabana"
+    }
+]
+
 app.get("/", (req, res) => {
     res.status(200).send("curos de node,js utilizando o express")
+})
+
+app.get("/livros", (req, res) => {
+    res.status(200).json(livros);
 })
 
 export default app;

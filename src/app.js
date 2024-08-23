cb7f7faaf39a04651a33637c8026b78291c610d7
Gustavo Menezes <gustavo.menezes@bistwobis.com.br>
2024-08-23 16:05:35 -0300

    creating function findBook to get by id, and the method put using postman to send

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ import express from "express";
 const app = express();
 app.use(express.json());
 
-const livros = [
+const books = [
     {
         id: 1,
         titulo: "Casais inteligentes enriquecem juntos"
@@ -14,17 +14,34 @@ const livros = [
     }
 ]
 
+function findBook(id) {
+    return books.findIndex(book => {
+        return book.id === Number(id);
+    })
+}
+
 app.get("/", (req, res) => {
     res.status(200).send("curos de node,js utilizando o express")
 })
 
 app.get("/livros", (req, res) => {
-    res.status(200).json(livros);
+    res.status(200).json(books);
+})
+
+app.get("/livros/:id", (req, res) => {
+    const index = findBook(req.params.id);
+    res.status(200).json(books[index]);
 })
 
 app.post("/livros", (req, res) => {
-    livros.push(req.body);
+    books.push(req.body);
     res.status(201).send("livro cadastrado com sucesso")
+})
+
+app.put("/livros/:id", (req, res) => {
+    const index = findBook(req.params.id);
+    books[index].titulo = req.body.titulo;
+    res.status(200).json(books);
 })
 
 export default app;
